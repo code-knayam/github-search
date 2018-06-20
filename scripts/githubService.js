1948fcb03428ce8f18c8809735fc4f4a33685173
code-knayam <mayankjain1394@gmail.com>
2018-06-20 21:04:17 +0530

    Added OAuth call to API.

--- scripts/githubService.js
+++ scripts/githubService.js
@@ -9,7 +9,7 @@ app.factory('GithubService', function ($http, $log, $q) {
     $http({
       method: 'GET',
       accept: 'application/vnd.github.v3+json',
-      url: 'https://api.github.com/search/users?q=' + userName + '&per_page=50'
+      url: 'https://api.github.com/search/users?q=' + userName + '&per_page=50' + '?client_id=a8f668093078fab675d7&client_secret=6db1684b289e0979bb41a0a18f943f79dc192100'
     }).then(function (response) {
       $log.info('[GithubService] User Search API Response ', response);
       
@@ -28,19 +28,19 @@ app.factory('GithubService', function ($http, $log, $q) {
 
     var userDetails$ = $q.defer();
 
-    // $http({
-    //   method: 'GET',
-    //   accept: 'application/vnd.github.v3+json',
-    //   url: 'https://api.github.com/users/' + userName
-    // }).then(function (response) {
-    //   $log.info('[GithubService] User Info API Response', response);
-    //   userDetails$.resolve(response.data);
-    // }, function (err) {
-    //   $log.error('[GithubService] User Info API Error ', err);
-    //   userDetails$.reject(err);
-    //   });
+    $http({
+      method: 'GET',
+      accept: 'application/vnd.github.v3+json',
+      url: 'https://api.github.com/users/' + userName + '?client_id=a8f668093078fab675d7&client_secret=6db1684b289e0979bb41a0a18f943f79dc192100'
+    }).then(function (response) {
+      $log.info('[GithubService] User Info API Response', response);
+      userDetails$.resolve(response.data);
+    }, function (err) {
+      $log.error('[GithubService] User Info API Error ', err);
+      userDetails$.reject(err);
+      });
     
-    userDetails$.resolve(userDetailsStubResponse);
+    // userDetails$.resolve(userDetailsStubResponse);
 
     return userDetails$.promise;
   }
@@ -51,19 +51,19 @@ app.factory('GithubService', function ($http, $log, $q) {
 
     var userRepos$ = $q.defer();
 
-    // $http({
-    //   method: 'GET',
-    //   accept: 'application/vnd.github.v3+json',
-    //   url: 'https://api.github.com/users/' + userName + '/repos'
-    // }).then(function (response) {
-    //   $log.info('[GithubService] User Repos API Response', response);
-    //   userRepos$.resolve(response.data);
-    // }, function (err) {
-    //   $log.error('[GithubService]  User Repos API Error ', err);
-    //   userRepos$.reject(err);
-    //   });
+    $http({
+      method: 'GET',
+      accept: 'application/vnd.github.v3+json',
+      url: 'https://api.github.com/users/' + userName + '/repos' + '?client_id=a8f668093078fab675d7&client_secret=6db1684b289e0979bb41a0a18f943f79dc192100'
+    }).then(function (response) {
+      $log.info('[GithubService] User Repos API Response', response);
+      userRepos$.resolve(response.data);
+    }, function (err) {
+      $log.error('[GithubService]  User Repos API Error ', err);
+      userRepos$.reject(err);
+      });
 
-    userRepos$.resolve(userReposStubResponse);
+    // userRepos$.resolve(userReposStubResponse);
     
     return userRepos$.promise;
   };
@@ -74,19 +74,19 @@ app.factory('GithubService', function ($http, $log, $q) {
 
     var contributors$ = $q.defer();
 
-    // $http({
-    //   method: 'GET',
-    //   'accept': 'application/vnd.github.v3+json',
-    //   url: 'https://api.github.com/repos/' + userName + '/' + repoName + '/stats/contributors'
-    // }).then(function (response) {
-    //   $log.info('[GithubService] Contributors API Response', response);
-    //   contributors$.resolve(response);
-    // }, function (err) {
-    //   $log.error('[GithubService]  Contributors API Error ', err);
-    //   contributors$.reject(err);
-    //   });
+    $http({
+      method: 'GET',
+      accept: 'application/vnd.github.v3+json',
+      url: 'https://api.github.com/repos/' + userName + '/' + repoName + '/stats/contributors' + '?client_id=a8f668093078fab675d7&client_secret=6db1684b289e0979bb41a0a18f943f79dc192100'
+    }).then(function (response) {
+      $log.info('[GithubService] Contributors API Response', response);
+      contributors$.resolve(response.data);
+    }, function (err) {
+      $log.error('[GithubService]  Contributors API Error ', err);
+      contributors$.reject(err);
+      });
 
-    contributors$.resolve(userContributorStubResponse);
+    // contributors$.resolve(userContributorStubResponse);
     
     return contributors$.promise;
   }
@@ -96,19 +96,19 @@ app.factory('GithubService', function ($http, $log, $q) {
 
     var commitActivity$ = $q.defer();
 
-    // $http({
-    //   method: 'GET',
-    //   'accept': 'application/vnd.github.v3+json',
-    //   url: 'https://api.github.com/repos/' + userName + '/' + repoName + '/stats/commit_activity'
-    // }).then(function (response) {
-    //   $log.info('[GithubService] Commit Activity API Response', response);
-    //   commitActivity$.resolve(response);
-    // }, function (err) {
-    //   $log.error('[GithubService] Commit Activity API Error ', err);
-    //   usercommitActivityRepos$.reject(err);
-    //   });
+    $http({
+      method: 'GET',
+      accept: 'application/vnd.github.v3+json',
+      url: 'https://api.github.com/repos/' + userName + '/' + repoName + '/stats/commit_activity' + '?client_id=a8f668093078fab675d7&client_secret=6db1684b289e0979bb41a0a18f943f79dc192100'
+    }).then(function (response) {
+      $log.info('[GithubService] Commit Activity API Response', response);
+      commitActivity$.resolve(response.data);
+    }, function (err) {
+      $log.error('[GithubService] Commit Activity API Error ', err);
+      usercommitActivityRepos$.reject(err);
+      });
 
-    commitActivity$.resolve(commitActivityStubResponse);
+    // commitActivity$.resolve(commitActivityStubResponse);
     
     return commitActivity$.promise;
   }
