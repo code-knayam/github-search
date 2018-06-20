9be190dca4829a5f91f493631523ee120e0fb589
code-knayam <mayankjain1394@gmail.com>
2018-06-20 15:49:12 +0530

    Updated Logging statements.

--- scripts/githubService.js
+++ scripts/githubService.js
@@ -1,26 +1,28 @@
 app.factory('GithubService', function ($http, $log, $q) {
 
+  // API call to fetch User Search 
   _getUserList = function (userName) {
+    $log.info('[GithubService] Searching for User Name = ', userName);
 
-    $log.info('[GithubService] User Name = ', userName);
     var userData$ = $q.defer();
-    // API call
+    
     $http({
       method: 'GET',
       accept: 'application/vnd.github.v3+json',
       url: 'https://api.github.com/search/users?q=' + userName + '&per_page=50'
     }).then(function (response) {
-      $log.info('[GithubService] ', response);
+      $log.info('[GithubService] User Search API Response ', response);
       
       userData$.resolve(response.data);
     }, function (err) {
-      $log.error('[GithubService] ', err);
+      $log.error('[GithubService] User Search API Error ', err);
       userData$.reject(err);
       });
     
     return userData$.promise;
   }
 
+  // API call to fetch User Info
   _getUserInfo = function (userName) {
     $log.info('[GithubService] Fetching Info for User = ', userName);
 
@@ -31,16 +33,17 @@ app.factory('GithubService', function ($http, $log, $q) {
       accept: 'application/vnd.github.v3+json',
       url: 'https://api.github.com/users/' + userName
     }).then(function (response) {
-      $log.info('[GithubService] ', response);
+      $log.info('[GithubService] User Info API Response', response);
       userDetails$.resolve(response.data);
     }, function (err) {
-      $log.error('[GithubService] ', err);
+      $log.error('[GithubService] User Info API Error ', err);
       userDetails$.reject(err);
       });
     
     return userDetails$.promise;
   }
 
+  // API call to fetch User Repos
   _getUserRepos = function (userName) {
     $log.info('[GithubService] Fetching Repos for User = ', userName);
 
@@ -51,10 +54,10 @@ app.factory('GithubService', function ($http, $log, $q) {
       accept: 'application/vnd.github.v3+json',
       url: 'https://api.github.com/users/' + userName + '/repos'
     }).then(function (response) {
-      $log.info('[GithubService] ', response);
+      $log.info('[GithubService] User Repos API Response', response);
       userRepos$.resolve(response.data);
     }, function (err) {
-      $log.error('[GithubService] ', err);
+      $log.error('[GithubService]  User Repos API Error ', err);
       userRepos$.reject(err);
       });
     
