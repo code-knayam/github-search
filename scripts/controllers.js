6bcdbd86fead6757caf64ad0c19d71719ce6cc1a
code-knayam <mayankjain1394@gmail.com>
2018-06-20 13:17:18 +0530

    Added code comments.

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,15 +1,21 @@
+// Controller for Search Page
 app.controller('searchUserController', function ($scope, $log, $location, GithubService) {
 
+  // When Search Button Clicked
   $scope.onSubmit = function () {
 
+    // showing spinner indicating start of search
     $scope.showSpinner = true;
     
     $log.info('[searchUserController] User Name = ', $scope.userName);
 
     var userName = $scope.userName;
+
+    // Fetching User search results
     GithubService.getUserList(userName)
       .then(function (data) {
         $log.info('[searchUserController] User Names Fetch - Data from Service', data);
+        // Fading In search result DIV to show results
         $('.search-result').fadeIn(100);
         showUserSearchResult(data);
       }, function (err) {
@@ -17,17 +23,26 @@ app.controller('searchUserController', function ($scope, $log, $location, Github
       });
   }
   
+  // When View Details Button Clicked
   $scope.onViewDetails = function (userName) {
     $log.info('[searchUserController] Navigating to Details Page for User Name = ', userName);
+    // Scrolling page to Top
+    $('html, body').animate({ scrollTop: 0 }, 10);
+    // Navigating to Details Page
     $location.path('details/' + userName);
   }
 
+  // Show User Search Results
   function showUserSearchResult(data) {
     $scope.total_count = data.total_count;
+
+    // When no search results found
     if ($scope.total_count === 0) {
       $scope.showNotFoundMessage = true;
       $scope.users = null;
     } else {
+      // when users found
+      // showing extra info when search result greater than 50
       if ($scope.total_count > 50) {
         $scope.showExtraInfo = true;
       } else {
@@ -36,10 +51,12 @@ app.controller('searchUserController', function ($scope, $log, $location, Github
       $scope.showNotFoundMessage = false;
       $scope.users = data.items;
     }
+    // Hiding Spinner depicting results fetched
     $scope.showSpinner = false;
     scrollResultToTop();
   }
 
+  // Scrolling Search Result DIV to top
   function scrollResultToTop() {
     var top = $('.search-result').offset().top - 50;
     window.setTimeout(function () {
@@ -49,9 +66,10 @@ app.controller('searchUserController', function ($scope, $log, $location, Github
   
 });
 
-
+// Controller for User Details Page
 app.controller('userDetailController', function ($scope, $routeParams, $log, GithubService) {
   
+  // Getting UserName form RouteParams
   $scope.userName = $routeParams.userName;
   
   // GithubService.getUserInfo($scope.userName)
@@ -1895,14 +1913,18 @@ app.controller('userDetailController', function ($scope, $routeParams, $log, Git
   ];
 });
 
+// Controller for Repository Details
 app.controller('repoDetailController', function ($scope) {   
 
+  // Show API chart function based on index passed from DOM
   $scope.showApiChart = function (index) {
+    // Hiding all visible charts
     $('.chart-info').css('display', 'none');
-    console.log(index);
+    // Showing the nth index chart
     $('.chart-info:nth-child(' + index + ')').fadeIn(500);
   }
-  $scope.showApiChart(1);
 
+  // showing default chart on DOM load
+  $scope.showApiChart(1);
   
 })
